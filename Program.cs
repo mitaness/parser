9536a01e3c8d8eb4bcbada4e14d33b5dae872ae7
Your Name <you@example.com>
2025-04-05 10:27:14 +0200

    IResult<out T>

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace bar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+interface IResult<out T>
+{
+
+}
+
+interface IParser<out T>
+{
+    IResult<T> Parse(string input);
+}
+
+class Result<T> : IResult<T>
+{
+    T _value = default(T);
+    string _rest = string.Empty;
+    string _error = string.Empty;
+
+    public T Value => _value;
+    public string Rest => _rest;
+    public string Error => _error;
+
+    public bool HasValue { get; }
+
+    private Result(T value, string rest)
+    {
+        _value = value;
+        _rest = rest;
+        HasValue = true;
+    }
+
+    private Result(string error)
+    {
+        _error = error;
+    }
+
+    public static Result<T> Ok(T value, string rest)
+    {
+        return new Result<T>(value, rest);
+    }
+
+    public static Result<T> Fail(string error)
+    {
+        return new Result<T>(error);
+    }
+}
