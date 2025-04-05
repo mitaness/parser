b5bbf1bf5448a8e75bf7603a5881423b4157a239
Your Name <you@example.com>
2025-04-05 10:45:05 +0200

    2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,143 @@
-﻿namespace bar;
+﻿
+
+namespace bar;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Test3();
+    }
+
+    private static void Test3()
+    {
+        var A = new Cond(char.IsAsciiLetterLower);
+        var B = A.Parse("bAbc");
+    }
+
+    private static void Test2()
+    {
+        var A = new XParser('A');
+        var B = new XParser('B');
+        var C = new OrElse<char>(A, B);
+        var D = C.Parse("A0Babc");
+    }
+
+    private static void Test()
+    {
+        var A = new XParser('A');
+        var B = A.Parse("DAbc");
+    }
+}
+
+class ZeroMore<T> : IParser<T[]>
+{
+    public IParser<T> A { get; }
+
+    public IResult<T[]> Parse(string input)
+    {
+        var list = new List<T>();
+        var rest = input;
+
+        while (true)
+        {
+            var B = A.Parse(rest);
+
+            if (B.HasValue)
+            {
+                list.Add(B.Value);
+                //rest = B.;
+            }
+        }
+        throw new NotImplementedException();
+    }
+
+    public ZeroMore(IParser<T> a)
+    {
+        A = a;
+    }
+}
+
+class Cond : IParser<char>
+{
+    public Func<char, bool> Condition { get; }
+
+    public IResult<char> Parse(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return Result<char>.Fail("No more input");
+        }
+
+        var A = input.First();
+
+        if (Condition(A))
+        {
+            return Result<char>.Ok(A, input.Substring(1));
+        }
+
+        return Result<char>.Fail("Condition failed");
+    }
+
+    public Cond(Func<char, bool> condition)
+    {
+        Condition = condition;
+    }
+}
+
+class OrElse<T> : IParser<T>
+{
+    public IParser<T> A { get; }
+    public IParser<T> B { get; }
+
+    public IResult<T> Parse(string input)
+    {
+        var result = A.Parse(input);
+
+        if (result.HasValue)
+        {
+            return result;
+        }
+
+        return B.Parse(input);
+    }
+
+    public OrElse(IParser<T> a, IParser<T> b)
+    {
+        A = a;
+        B = b;
+    }
+}
+
+class XParser : IParser<char>
+{
+    public char X { get; }
+
+    public IResult<char> Parse(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return Result<char>.Fail("No more input");
+        }
+        var A = input.First();
+        if (A == X)
+        {
+            return Result<char>.Ok(A, input.Substring(1));
+        }
+
+        return Result<char>.Fail($"Expected {X}. Got {A}.");
+    }
+
+    public XParser(char x)
+    {
+        X = x;
     }
 }
 
 interface IResult<out T>
 {
-
+    T Value { get; }
+    bool HasValue { get; }
 }
 
 interface IParser<out T>
